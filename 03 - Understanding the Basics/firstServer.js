0a2cc61b00879d2b72dbf601788155224957a084
jamesacox53 <jamesacox53@gmail.com>
2023-09-23 17:39:52 +0100

    Completed Routing Requests

--- 03 - Understanding the Basics/firstServer.js
+++ 03 - Understanding the Basics/firstServer.js
@@ -1,8 +1,27 @@
 const http = require('http');
 
 function serverListener(request, response) {
-  console.log(request);
+  const url = request.url;
 
+  if (url == '/') {
+    return sendInputFormPage(response);
+
+  } else {
+    return sendDefaultPage(response);
+  }
+}
+
+function sendInputFormPage(response) {
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<html>');
+  response.write('<head><title>My First HTML Page</title></head>');
+  response.write('<body><form action="/message" method="POST"><input type="text" name="messageText"><button type="submit">Send</button></form></body>');
+  response.write('</html>');
+
+  return response.end();
+}
+
+function sendDefaultPage(response) {
   response.setHeader('Content-Type', 'text/html');
   response.write('<html>');
   response.write('<head><title>My First HTML Page</title></head>');
