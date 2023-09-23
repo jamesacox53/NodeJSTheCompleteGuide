c231292836ff8b54ded9449b73c76f6ea4c36ac7
jamesacox53 <jamesacox53@gmail.com>
2023-09-23 17:21:05 +0100

    Completed Sending Responses

--- 03 - Understanding the Basics/firstServer.js
+++ 03 - Understanding the Basics/firstServer.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 function serverListener(request, response) {
   console.log(request);
+
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<html>');
+  response.write('<head><title>My First HTML Page</title></head>');
+  response.write('<body><h1>Hello from my Node.js Server</h1></body>');
+  response.write('</html>');
+  response.end();
 }
 
 const server = http.createServer(serverListener);
