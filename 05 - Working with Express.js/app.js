248049d7fcd0b9e021a3713ebdd4943a9015a88c
jamesacox53 <jamesacox53@gmail.com>
2023-10-01 18:31:05 +0100

    Completed How Middleware Works

--- 05 - Working with Express.js/app.js
+++ 05 - Working with Express.js/app.js
@@ -4,8 +4,17 @@ const http = require('http');
 // 3rd Party
 const express = require('express');
 
-
 const app = express();
+
+app.use((request, response, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((request, response, next) => {
+    console.log('In the next middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
