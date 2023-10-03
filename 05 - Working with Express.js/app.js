94b84ce40b2f33cc5b78c3a3d7a616e6d611465c
jamesacox53 <jamesacox53@gmail.com>
2023-10-03 21:39:03 +0100

    Completed Parsing Incoming Requests

--- 05 - Working with Express.js/app.js
+++ 05 - Working with Express.js/app.js
@@ -3,11 +3,19 @@ const http = require('http');
 
 // 3rd Party
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (request, response, next) => {
-    response.send('<h1>The Product Page</h1>');
+    response.send('<form action="/product" method="POST"><input type="text" name="messageText"><button type="submit">Send</button></form>');
+});
+
+app.post('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
 });
 
 app.use('/', (request, response, next) => {
