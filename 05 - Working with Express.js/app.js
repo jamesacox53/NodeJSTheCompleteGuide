0c5e47b76f4f52690b5dae2c565945094fe14298
jamesacox53 <jamesacox53@gmail.com>
2023-11-12 20:53:54 +0000

    Completed Filtering Paths

--- 05 - Working with Express.js/app.js
+++ 05 - Working with Express.js/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
 
--- 05 - Working with Express.js/routes/admin.js
+++ 05 - Working with Express.js/routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (request, response, next) => {
-    response.send('<form action="/product" method="POST"><input type="text" name="messageText"><button type="submit">Send</button></form>');
+    response.send('<form action="/admin/product" method="POST"><input type="text" name="messageText"><button type="submit">Send</button></form>');
 });
 
 router.post('/product', (request, response, next) => {
