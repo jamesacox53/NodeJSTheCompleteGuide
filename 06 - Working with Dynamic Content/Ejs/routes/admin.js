70668384920e9c46866c0141a45811b5c52537d2
jamesacox53 <jamesacox53@gmail.com>
2023-11-23 23:25:27 +0000

    Completed Working with EJS

---
+++ 06 - Working with Dynamic Content/Ejs/routes/admin.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+const rootDirectoryStr = require('../util/rootDirectory.js');
+const productsArr = require('../database/productsArr.js');
+
+router.get('/admin/add-product', (request, response, next) => {
+  var optionsObj = {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  };
+
+  response.render('add-product.ejs', optionsObj);
+});
+
+router.post('/admin/add-product', (request, response, next) => {
+  var productDetailsObj = {
+    title: request.body.title
+  };
+
+  console.log(productDetailsObj.title);
+
+  productsArr.push(productDetailsObj);
+  response.redirect('/');
+});
+
+module.exports = router;
---
+++ 06 - Working with Dynamic Content/Ejs/routes/shop.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+const rootDirectoryStr = require('../util/rootDirectory');
+const productsArr = require('../database/productsArr.js');
+
+router.get('/', (request, response, next) => {
+  var optionsObj = {
+    pageTitle: 'Shop',
+    path: '/',
+    hasProds: (productsArr.length > 0),
+    prods: productsArr,
+    activeShop: true,
+    productCSS: true
+  };
+
+  response.render('shop.ejs', optionsObj);
+});
+
+module.exports = router;
