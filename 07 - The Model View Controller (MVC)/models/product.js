14c3f3f7286d1f91062ce7b152243df420111a93
jamesacox53 <jamesacox53@gmail.com>
2023-11-28 20:59:23 +0000

    Completed Storing Data in Files Via the Model

--- 07 - The Model View Controller (MVC)/models/product.js
+++ 07 - The Model View Controller (MVC)/models/product.js
@@ -1,4 +1,8 @@
-const productsArr = require('../database/productsArr.js');
+const fs = require('fs');
+const path = require('path');
+
+const rootDirectoryStr = require(path.join('..', 'util', 'rootDirectory.js'));
+const productsFilePath = path.join(rootDirectoryStr, 'data', 'products.json');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +10,43 @@ module.exports = class Product {
     }
 
     save() {
-        productsArr.push(this);
+        fs.readFile(productsFilePath, (error, fileContent) => {
+            return this._readFileForSave(this, error, fileContent);
+        });
     }
 
     static fetchAll() {
+        fs.readFile(productsFilePath, (error, fileContent) => {
+            return this._readFileForFetchAll(error, fileContent);
+        });
+    }
+
+    _readFileForSave(product, error, fileContent) {
+        let productsArr;
+        
+        if (error) {
+            productsArr = [];
+        
+        } else {
+            productsArr = JSON.parse(fileContent);
+        }
+
+        productsArr.push(product);
+
+        var productsArrJSONStr = JSON.stringify(productsArr);
+        fs.writeFile(productsFilePath, productsArrJSONStr, this._failedToWriteFileForSave);
+    }
+
+    _failedToWriteFileForSave(error) {
+        if (!error) return;
+
+        console.log(error);
+    }
+
+    _readFileForFetchAll(error, fileContent) {
+        if (error) return [];
+        
+        const productsArr = JSON.parse(fileContent);
         return productsArr;
     }
 }
