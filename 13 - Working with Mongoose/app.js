67c04565636763a25d945631f130b0ee24d7d9be
jamesacox53 <jamesacox53@gmail.com>
2023-12-25 12:53:44 +0000

    Completed Connecting to the MongoDB Server with Mongoose

--- 13 - Working with Mongoose/app.js
+++ 13 - Working with Mongoose/app.js
@@ -5,40 +5,29 @@ const path = require('path');
 // 3rd Party
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const rootDirectoryStr = path.dirname(require.main.filename);
-const mongoDatabase = require(path.join(rootDirectoryStr, 'util', 'mongoDBCreds.js'));
-const mongoConnect = mongoDatabase.mongoConnect;
+const connectionStr = require(path.join(rootDirectoryStr, 'database', 'mongooseDBCreds.js'));
 
-// const sequelize = require(path.join(rootDirectoryStr, 'util', 'mySqlDatabaseCreds.js'));
-// const sequelizeAssociations = require(path.join(rootDirectoryStr, 'models', 'associations.js'));
-const userMiddleware = require(path.join(rootDirectoryStr, 'util', 'userMiddleware.js'));
-
-const adminRoutes = require(path.join(rootDirectoryStr, 'routes', 'adminRoutes.js'));
-const shopRoutes = require(path.join(rootDirectoryStr, 'routes', 'shopRoutes.js'));
+// const userMiddleware = require(path.join(rootDirectoryStr, 'util', 'userMiddleware.js'));
+// const adminRoutes = require(path.join(rootDirectoryStr, 'routes', 'adminRoutes.js'));
+// const shopRoutes = require(path.join(rootDirectoryStr, 'routes', 'shopRoutes.js'));
 // const errorRoutes = require(path.join(rootDirectoryStr, 'routes', 'errorRoutes.js'));
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDirectoryStr, 'public')));
-app.use(userMiddleware);
-
-app.use(adminRoutes);
-app.use(shopRoutes);
+// app.use(userMiddleware);
+// app.use(adminRoutes);
+// app.use(shopRoutes);
 // app.use(errorRoutes);
-
 
 const server = http.createServer(app);
 
-/*
-sequelize.sync()
-// sequelize.sync({ force: true })
-.then(res => server.listen(3000))
-.catch(err => console.log(err));
-*/
-
-mongoConnect(() => server.listen(3000));
+mongoose.connect(connectionStr)
+.then(res => server.listen(3000));
