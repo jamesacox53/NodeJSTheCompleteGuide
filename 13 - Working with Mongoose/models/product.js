4bd25e9a959ef67f39ff773189ed60c3db773fb0
jamesacox53 <jamesacox53@gmail.com>
2023-12-25 13:15:11 +0000

    Completed Creating the Product Schema

--- 13 - Working with Mongoose/models/product.js
+++ 13 - Working with Mongoose/models/product.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        required: true
+    },
+});
+/*
 const path = require('path');
 const mongodb = require('mongodb');
 
@@ -108,3 +131,4 @@ class Product {
 }
 
 module.exports = Product;
+*/
