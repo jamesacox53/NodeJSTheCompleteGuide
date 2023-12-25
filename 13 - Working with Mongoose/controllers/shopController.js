fd1cab2d32757e04aff5979bfe460f528b351f38
jamesacox53 <jamesacox53@gmail.com>
2023-12-25 12:43:13 +0000

    Completed What is Mongoose?

---
+++ 13 - Working with Mongoose/controllers/shopController.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const Product = require(path.join('..', 'models', 'product.js'));
+// const Order = require(path.join('..', 'models', 'order.js'));
+// const CartItem = require(path.join('..', 'models', 'cart-item.js'));
+// const OrderItem = require(path.join('..', 'models', 'order-item.js'));
+
+exports.getProducts = (request, response, next) => {
+  Product.fetchAll()
+  .then(arr => _getProducts(arr))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'All Products',
+      pathStr: '/products',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'product-list.ejs'), optionsObj);
+  }
+};
+
+exports.getProduct = (request, response, next) => {
+  const productID = request.params.productID;
+  
+  Product.findById(productID)
+  .then(product => _renderProductDetailsPage(product, response))
+  .catch(err => console.log(err));
+  
+  function _renderProductDetailsPage (product, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: product.title,
+      pathStr: '/products',
+      product: product
+    };
+
+    response.render(path.join('shop', 'product-detail.ejs'), optionsObj);
+  }
+};
+
+exports.getIndex = (request, response, next) => {
+  Product.fetchAll()
+  .then(arr => _getProducts(arr, response))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Shop',
+      pathStr: '/',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'index.ejs'), optionsObj);
+  }
+};
+
+exports.getCart = (request, response, next) => {
+  request.user.getCart()
+  .then(cartProducts => _renderCartPage(cartProducts))
+  .catch(err => console.log(err));
+  
+  function _renderCartPage(cartProducts) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Your Cart',
+      pathStr: '/cart',
+      productsArr: cartProducts
+    };
+    
+    response.render(path.join('shop', 'cart.ejs'), optionsObj);
+  }
+};
+
+exports.postCart = (request, response, next) => {
+  const productID = request.body.productID;
+  
+  Product.findById(productID)
+  .then(product => request.user.addToCart(product))
+  .then(err => _redirectToCartPage(response))
+  .catch(err => console.log(err));
+
+  function _redirectToCartPage(response) {
+    response.redirect('/cart');
+  }
+};
+
+exports.postCartDeleteItem = (request, response, next) => {
+  const productID = request.body.productID;
+
+  request.user.deleteCartItem(productID)
+  .then(err => _redirectToCart(response))
+  .catch(err => console.log(err));
+
+  function _redirectToCart(response) {
+    response.redirect('/cart');
+  }
+};
+
+exports.getOrders = (request, response, next) => {
+  request.user.getOrders()
+  .then(ordersArr => _renderOrdersPage(ordersArr))
+  .catch(err => console.log(err))
+  
+  function _renderOrdersPage(ordersArr) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Your Orders',
+      pathStr: '/orders',
+      ordersArr: ordersArr
+    };
+  
+    response.render(path.join('shop', 'orders.ejs'), optionsObj);
+  }
+};
+
+exports.postOrder = (request, response, next) => {
+  request.user.addOrder()
+  .then(err => _redirectToOrdersPage(response))
+  .catch(err => console.log(err));
+
+  function _redirectToOrdersPage(response) {
+    response.redirect('/orders');
+  }
+};
+
+exports.getCheckout = (request, response, next) => {
+  Product.findAll()
+  .then(arr => _getProducts(arr, response))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Checkout',
+      pathStr: '/checkout',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'checkout.ejs'), optionsObj);
+  }
+};
---
+++ 13 - Working with Mongoose/models/user.js
@@ -0,0 +1,284 @@
+const path = require('path');
+const mongodb = require('mongodb');
+
+const rootDirectoryStr = path.dirname(require.main.filename);
+const mongoDatabase = require(path.join(rootDirectoryStr, 'util', 'mongoDBCreds.js'));
+const getDB = mongoDatabase.getDB;
+
+class User {
+    constructor(argsObj) {
+        this.username = argsObj.username;
+        this.email = argsObj.email;
+        
+        if (argsObj._id)
+            this._id = argsObj._id;
+
+        if (argsObj.cart) {
+            this.cart = argsObj.cart;
+        
+        } else {
+            this.cart = {
+                products: []
+            };
+        }
+    }
+
+    save() {
+        const db = getDB();
+        const argsObj = this._getArgsObj();
+
+        if (argsObj._id) {
+            return this.constructor._updateUser(db, argsObj);
+        
+        } else {
+            return this.constructor._insertUser(db, argsObj);
+        }
+    }
+
+    _getArgsObj() {
+        const argsObj = {
+            username: this.username,
+            email: this.email,
+            cart: this.cart
+        };
+
+        if (this._id)
+            argsObj['_id'] = this._id;
+
+        return argsObj;
+    }
+
+    static _updateUser(db, argsObj) {
+        const mongoIDObj = new mongodb.ObjectId(argsObj._id);
+        const filterObj = { _id: mongoIDObj };
+        const updateObj = { $set: argsObj };
+
+        return db.collection('users').updateOne(filterObj, updateObj);
+    }
+
+    static _insertUser(db, argsObj) {
+        return db.collection('users').insertOne(argsObj);
+    }
+
+    static findById(userID) {
+        const db = getDB();
+        const mongoIDObj = new mongodb.ObjectId(userID);
+        
+        return db.collection('users')
+        .findOne({ _id: mongoIDObj })
+        .then(mongoUser => this._getUser(mongoUser));
+    }
+
+    static _getUser(mongoUser) {
+        if (!mongoUser) return null;
+        
+        const argsObj = {
+            username: mongoUser.username,
+            email: mongoUser.email,
+            cart: mongoUser.cart
+        };
+
+        if (mongoUser._id)
+            argsObj['_id'] = mongoUser._id;
+
+        return new User(argsObj);
+    }
+
+    static deleteById(userID) {
+        const db = getDB();
+        const mongoIDObj = new mongodb.ObjectId(userID);
+        
+        return db.collection('users').deleteOne({ _id: mongoIDObj });
+    }
+
+    getCart() {
+        return this.constructor
+        ._getProducts(this.cart)
+        .toArray()
+        .then(products => this.constructor._getProductObjArr(products, this.cart));
+    }
+
+    static _getProducts(cart) {
+        const db = getDB();
+        const productIDArr = this._getCartProductIDArr(cart);
+
+        
+        return db.collection('products').find({ _id: { $in: productIDArr } });
+    }
+
+    static _getCartProductIDArr(cart) {
+        const productIDArr = [];
+        const productsArr = cart.products;
+        
+        for (let i = 0; i < productsArr.length; i++) {
+            const productObj = productsArr[i];
+            const productID = productObj.productID;
+
+            productIDArr.push(productID);
+        }
+
+        return productIDArr;
+    }
+
+    static _getProductObjArr(products, cart) {
+        const productObjArr = [];
+        const cartProductsArr = cart.products;
+        const productsObj = this._getObjFromArr(products);
+
+        for(let i = 0; i < cartProductsArr.length; i++) {
+            const cartProduct = cartProductsArr[i];
+            const product = productsObj[cartProduct.productID.toString()];
+
+            const productObj = {
+                product: product,
+                quantity: cartProduct.quantity
+            };
+
+            productObjArr.push(productObj);
+        }
+
+        return productObjArr;
+    }
+
+    static _getObjFromArr(arr) {
+        const retObj = {};
+
+        for(let i = 0; i < arr.length; i++) {
+            const elem = arr[i];
+            
+            retObj[elem._id.toString()] = elem;
+        }
+
+        return retObj;
+    }
+
+    addToCart(product) {
+        const productsArr = this.cart.products;
+        const productIndex = this.constructor._getProductIndex(product, productsArr);
+
+        if (productIndex == -1) {
+            return this.constructor._addProductToCart(this._id, product, this.cart);
+        
+        } else {
+            return this.constructor._updateProductQuantityInCart(this._id, productIndex, this.cart);
+        }
+    }
+
+    static _getProductIndex(product, productsArr) {
+        const productIDStr = product._id.toString();
+        
+        for (let i = 0; i < productsArr.length; i++) {
+            const prodObj = productsArr[i];
+            const prodIDStr = prodObj.productID.toString();
+            
+            if (prodIDStr == productIDStr)
+                return i;
+        }
+
+        return -1;
+    }
+
+    static _addProductToCart(_id, product, cart) {
+        const productsArr = cart.products;
+        const productObj = {
+            productID: product._id,
+            quantity: 1
+        };
+
+        productsArr.push(productObj);
+
+        return this._updateUserCartInDatabase(_id, cart);
+    }
+
+    static _updateUserCartInDatabase(_id, cart) {
+        const db = getDB();
+        const mongoIDObj = new mongodb.ObjectId(_id);
+        const filterObj = { _id: mongoIDObj };
+        const updateObj = {
+            $set: {
+                cart: cart
+            }
+        };
+
+        return db.collection('users').updateOne(filterObj, updateObj);
+    }
+
+    static _updateProductQuantityInCart(_id, productIndex, cart) {
+        const productsArr = cart.products;
+        const productObj = productsArr[productIndex];
+        
+        const oldQuantity = parseInt(productObj['quantity'], 10);
+        const newQuantity = oldQuantity + 1; 
+        
+        productObj['quantity'] = newQuantity;
+
+        return this._updateUserCartInDatabase(_id, cart);
+    }
+
+    deleteCartItem(productID) {
+        this.constructor._removeCartItem(this.cart, productID);
+
+        return this.constructor._updateUserCartInDatabase(this._id, this.cart);
+    }
+
+    static _removeCartItem(cart, productID) {
+        const newProductsArr = [];
+        const products = cart.products;
+        const productIDStr = productID.toString();
+
+        for(let i = 0; i < products.length; i++) {
+            const prod = products[i];
+            const prodIDStr = prod.productID.toString();
+
+            if (productIDStr != prodIDStr)
+                newProductsArr.push(prod);
+        }
+
+        cart.products = newProductsArr;
+    }
+
+    addOrder() {
+        return this.getCart()
+        .then(productObjArr => this.constructor._addOrder(this, productObjArr))
+        .then(err => this.constructor._resetCart(this));
+    }
+
+    static _addOrder(user, productObjArr) {
+        const db = getDB();
+        const orderObj = {
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            },
+            cart: productObjArr
+        }
+
+        return db.collection('orders').insertOne(orderObj);
+    }
+
+    static _resetCart(user) {
+        const newCart = {
+            products: []
+        };
+
+        user.cart = newCart;
+        
+        return this._updateUserCartInDatabase(user._id, user.cart);
+    }
+
+    getOrders() {
+        const db = getDB();
+        const mongoIDObj = new mongodb.ObjectId(this._id);
+
+        const queryObj = {
+            'user._id': mongoIDObj
+        };
+
+        return db.collection('orders')
+        .find(queryObj)
+        .toArray();
+    }
+}
+
+module.exports = User;
