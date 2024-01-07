9c50c1b14de9eaf1dd0124e095ea416062a1b5fb
jamesacox53 <jamesacox53@gmail.com>
2024-01-07 14:45:14 +0000

    Completed Module Introduction

---
+++ 16 - Sending Emails/controllers/authController.js
@@ -0,0 +1,147 @@
+const path = require('path');
+const bcryptjs = require('bcryptjs');
+
+const rootDirectoryStr = path.dirname(require.main.filename);
+const User = require(path.join(rootDirectoryStr, 'models', 'user.js'));
+
+exports.getLoginPage = (request, response, next) => {
+  const optionsObj = {
+    path: path,
+    pageTitle: 'Login',
+    pathStr: '/login',
+    userDoesntExistErr: _getErrorMsg('userDoesntExistErr', request)
+  };
+   
+  response.render(path.join('auth', 'login.ejs'), optionsObj);
+
+  function _getErrorMsg(errorKey, request) {
+    if (!errorKey || !request) return null;
+
+    const errorArr = request.flash(errorKey);
+    if(errorArr.length < 1) return null;
+
+    return errorArr[0];
+  }
+};
+
+exports.getSignupPage = (request, response, next) => {
+  const optionsObj = {
+    path: path,
+    pageTitle: 'Sign Up',
+    pathStr: '/signup',
+    userAlreadyExistErr: _getErrorMsg('userAlreadyExistErr', request)
+  };
+   
+  response.render(path.join('auth', 'signup.ejs'), optionsObj);
+
+  function _getErrorMsg(errorKey, request) {
+    if (!errorKey || !request) return null;
+
+    const errorArr = request.flash(errorKey);
+    if(errorArr.length < 1) return null;
+
+    return errorArr[0];
+  }
+};
+
+exports.postLogin = (request, response, next) => {
+  const loginObj = {
+    emailStr: request.body.email,
+    passwordStr: request.body.password
+  };
+  
+  User.findOne({ 'email': loginObj.emailStr })
+  .then(user => _postLogin(user, loginObj, request, response))
+  
+  function _postLogin(user, loginObj, request, response) {
+    if (!user) {
+      request.flash('userDoesntExistErr', 'Invalid Email Address or Password');
+      return response.redirect('/login');
+    }
+
+    return _isCorrectPassword(user, loginObj)
+    .then(isCorrect => _loginOrRedirect(isCorrect, user, request, response));
+  }
+
+  function _isCorrectPassword(user, loginObj) {
+    const userPasswordHashStr = user.password;
+    const loginPasswordStr = loginObj.passwordStr;
+
+    return bcryptjs.compare(loginPasswordStr, userPasswordHashStr);
+  }
+
+  function _loginOrRedirect(isCorrect, user, request, response) {
+    if (!isCorrect) {
+      request.flash('userDoesntExistErr', 'Invalid Email Address or Password');
+      return response.redirect('/login');
+    }
+    
+    return _storeUserInSession(user, request, response);
+  }
+
+  function _storeUserInSession(user, request, response) {
+    request.session.user = user;
+    request.session.isAuthenticated = true;
+    request.session.isLoggedIn = true;
+    return request.session.save((err) => {
+      response.redirect('/');
+    });
+  }
+};
+
+exports.postSignup = (request, response, next) => {
+  const signupObj = {
+    emailStr: request.body.email,
+    passwordStr: request.body.password,
+    confirmPasswordStr: request.body.confirmPassword
+  };
+  
+  User.findOne({ email: signupObj.emailStr })
+  .then(user => _postSignup(user, signupObj, request, response))
+  .catch(err => console.log(err));
+
+  function _postSignup(user, signupObj, request, response) {
+    if (user) {
+      request.flash('userAlreadyExistErr', 'Email Address already exists');
+      return response.redirect('/signup');
+    }
+
+    return _getHashedPassword(signupObj)
+    .then(hashedPasswordStr => _createUser(hashedPasswordStr, signupObj))
+    .then(user => _storeUserInSession(user, request, response))
+    .catch(err => console.log(err));
+  }
+
+  function _getHashedPassword(signupObj) {
+    const passwordStr = signupObj.passwordStr;
+
+    return bcryptjs.hash(passwordStr, 12);
+  }
+
+  function _createUser(hashedPasswordStr, signupObj) {
+    const user = new User({
+      email: signupObj.emailStr,
+      password: hashedPasswordStr,
+      cart: {
+        items: []
+      }
+    });
+    
+    return user.save();
+  }
+
+  function _storeUserInSession(user, request, response) {
+    request.session.user = user;
+    request.session.isAuthenticated = true;
+    request.session.isLoggedIn = true;
+    return request.session.save((err) => {
+      response.redirect('/');
+    });
+  }
+};
+
+exports.postLogout = (request, response, next) => {
+  request.session.destroy(() => {
+    response.redirect('/');
+  })
+};
---
+++ 16 - Sending Emails/controllers/shopController.js
@@ -0,0 +1,153 @@
+const path = require('path');
+const Product = require(path.join('..', 'models', 'product.js'));
+const Order = require(path.join('..', 'models', 'order.js'));
+
+exports.getProducts = (request, response, next) => {
+  Product.find()
+  .then(arr => _getProducts(arr))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'All Products',
+      pathStr: '/products',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'product-list.ejs'), optionsObj);
+  }
+};
+
+exports.getProduct = (request, response, next) => {
+  const productID = request.params.productID;
+  
+  Product.findById(productID)
+  .then(product => _renderProductDetailsPage(product, response))
+  .catch(err => console.log(err));
+  
+  function _renderProductDetailsPage (product, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: product.title,
+      pathStr: '/products',
+      product: product
+    };
+
+    response.render(path.join('shop', 'product-detail.ejs'), optionsObj);
+  }
+};
+
+exports.getIndex = (request, response, next) => {
+  Product.find()
+  .then(arr => _getProducts(arr, response))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Shop',
+      pathStr: '/',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'index.ejs'), optionsObj);
+  }
+};
+
+exports.getCart = (request, response, next) => {
+  request.user.getCart()
+  .then(cart => _renderCartPage(cart))
+  .catch(err => console.log(err));
+  
+  function _renderCartPage(cart) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Your Cart',
+      pathStr: '/cart',
+      cart: cart
+    };
+    
+    response.render(path.join('shop', 'cart.ejs'), optionsObj);
+  }
+};
+
+exports.postCart = (request, response, next) => {
+  const productID = request.body.productID;
+  
+  Product.findById(productID)
+  .then(product => request.user.addToCart(product))
+  .then(err => _redirectToCartPage(response))
+  .catch(err => console.log(err));
+
+  function _redirectToCartPage(response) {
+    response.redirect('/cart');
+  }
+};
+
+exports.postCartDeleteItem = (request, response, next) => {
+  const productID = request.body.productID;
+
+  request.user.deleteCartItem(productID)
+  .then(err => _redirectToCart(response))
+  .catch(err => console.log(err));
+
+  function _redirectToCart(response) {
+    response.redirect('/cart');
+  }
+};
+
+exports.getOrders = (request, response, next) => {
+  Order.find({ 'user.userID': request.user._id })
+  .then(ordersArr => _renderOrdersPage(ordersArr))
+  .catch(err => console.log(err))
+  
+  function _renderOrdersPage(ordersArr) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Your Orders',
+      pathStr: '/orders',
+      ordersArr: ordersArr
+    };
+  
+    response.render(path.join('shop', 'orders.ejs'), optionsObj);
+  }
+};
+
+exports.postOrder = (request, response, next) => {
+  request.user.populate('cart.items.productID')
+  .then(user => _createOrder(user))
+  .then(order => request.user.clearCart())
+  .then(err => _redirectToOrdersPage(response))
+  .catch(err => console.log(err));
+
+  function _createOrder(user) {
+    const orderObj = Order.getOrderConstructorObj(user);
+    
+    const order = new Order(orderObj);
+    return order.save();
+  }
+
+  function _redirectToOrdersPage(response) {
+    response.redirect('/orders');
+  }
+};
+
+/*
+exports.getCheckout = (request, response, next) => {
+  Product.findAll()
+  .then(arr => _getProducts(arr, response))
+  .catch(err => console.log(err));
+  
+  function _getProducts(arr, response) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Checkout',
+      pathStr: '/checkout',
+      prods: arr
+    };
+  
+    response.render(path.join('shop', 'checkout.ejs'), optionsObj);
+  }
+};
+*/
