025648c868717e94973f97d6dc210a444623f446
jamesacox53 <jamesacox53@gmail.com>
2024-01-13 16:40:36 +0000

    Completed Adding Validation to Login

---
+++ 18 - Understanding Validation/controllers/authControllers/loginController.js
@@ -0,0 +1,133 @@
+const path = require('path');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+const rootDirectoryStr = path.dirname(require.main.filename);
+const User = require(path.join(rootDirectoryStr, 'models', 'user.js'));
+
+exports.getLoginPage = (request, response, next) => {
+  const optionsObj = {
+    path: path,
+    pageTitle: 'Login',
+    pathStr: '/login',
+    errorMessage: _getErrorMsg('errorMessage', request),
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    fieldErrorsObj: {
+      email: false,
+      password: false
+    }
+  };
+
+  response.render(path.join('auth', 'login.ejs'), optionsObj);
+  
+  function _getErrorMsg(errorKey, request) {
+    if (!errorKey || !request) return null;
+ 
+    const errorArr = request.flash(errorKey);
+    if(errorArr.length < 1) return null;
+  
+    return errorArr[0];
+  }
+};
+
+exports.postLogin = (request, response, next) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return _renderLoginPage(errors.array());
+
+  } else {
+    return _login();
+  }
+  
+  function _renderLoginPage(errorsArr) {
+    const optionsObj = {
+      path: path,
+      pageTitle: 'Login',
+      pathStr: '/login',
+      errorMessage: _getErrorMsg(errorsArr),
+      oldInput: {
+        email: request.body.email,
+        password: request.body.password
+      },
+      fieldErrorsObj: _getFieldErrorsObj(errorsArr)
+    };
+         
+    return response.status(422).render(path.join('auth', 'login.ejs'), optionsObj);
+  }
+    
+  function _getErrorMsg(errorsArr) {
+    const messagesArr = [];
+    
+    for(let i = 0; i < errorsArr.length; i++) {
+      const messageStr = errorsArr[i].msg;
+    
+      messagesArr.push(messageStr);
+    }
+    
+    return messagesArr.join('. ');
+  }
+
+  function _getFieldErrorsObj(errorsArr) {
+    const fieldErrorsObj = {
+      email: false,
+      password: false
+    };
+  
+    for (let i = 0; i < errorsArr.length; i++) {
+      const errorObj = errorsArr[i];
+      const path = errorObj['path'];
+  
+      fieldErrorsObj[path] = true;
+    }
+  
+    return fieldErrorsObj;
+  }
+  
+  function _login() {
+    const loginObj = {
+      emailStr: request.body.email,
+      passwordStr: request.body.password
+    };
+        
+    return User.findOne({ 'email': loginObj.emailStr })
+    .then(user => _postLogin(user, loginObj))
+  }
+    
+  function _postLogin(user, loginObj) {
+    if (!user) {
+      request.flash('errorMessage', 'Invalid Email Address or Password');
+      return response.redirect('/login');
+    }
+  
+    return _isCorrectPassword(user, loginObj)
+    .then(isCorrect => _loginOrRedirect(isCorrect, user));
+  }
+  
+  function _isCorrectPassword(user, loginObj) {
+    const userPasswordHashStr = user.password;
+    const loginPasswordStr = loginObj.passwordStr;
+  
+    return bcryptjs.compare(loginPasswordStr, userPasswordHashStr);
+  }
+  
+  function _loginOrRedirect(isCorrect, user) {
+    if (!isCorrect) {
+      request.flash('errorMessage', 'Invalid Email Address or Password');
+      return response.redirect('/login');
+    }
+      
+    return _storeUserInSession(user);
+  }
+  
+  function _storeUserInSession(user) {
+    request.session.user = user;
+    request.session.isAuthenticated = true;
+    request.session.isLoggedIn = true;
+    return request.session.save((err) => {
+      response.redirect('/');
+    });
+  }
+};
--- 18 - Understanding Validation/routes/authRoutes.js
+++ 18 - Understanding Validation/routes/authRoutes.js
@@ -1,18 +1,20 @@
 const path = require('path');
 
-const authController = require(path.join('..', 'controllers', 'authController.js'));
+const authController = require(path.join('..', 'controllers', 'authControllers', 'authController.js'));
+const loginController = require(path.join('..', 'controllers', 'authControllers', 'loginController.js'));
+const signupController = require(path.join('..', 'controllers', 'authControllers', 'signupController.js'));
 const validators = require(path.join('..', 'validators', 'validators.js'));
 
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', authController.getLoginPage);
-router.get('/signup', authController.getSignupPage);
+router.get('/login', loginController.getLoginPage);
+router.get('/signup', signupController.getSignupPage);
 router.get('/reset/:token', authController.getResetPasswordPage);
 router.get('/reset', authController.getResetPage);
 
-router.post('/login', validators.loginValidatorsArr, authController.postLogin);
-router.post('/signup', validators.signupValidatorsArr, authController.postSignup);
+router.post('/login', validators.loginValidatorsArr, loginController.postLogin);
+router.post('/signup', validators.signupValidatorsArr, signupController.postSignup);
 router.post('/reset', authController.postReset);
 router.post('/logout', authController.postLogout);
 router.post('/new-password', authController.postNewPassword);
