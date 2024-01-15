896ca352dc99b2051955423741e4ed58f57e7326
jamesacox53 <jamesacox53@gmail.com>
2024-01-15 21:51:47 +0000

    Did some refactoring

--- 19 - Error Handling/controllers/adminController.js
+++ 19 - Error Handling/controllers/adminController.js
@@ -1,28 +1,15 @@
 const path = require('path');
 const { validationResult } = require('express-validator');
+
+const adminRenderer = require(path.join('..', 'viewRenderers', 'adminRenderer.js'));
 const Product = require(path.join('..', 'models', 'product.js'));
 
 exports.getAddProductPage = (request, response, next) => {
-  const optionsObj = {
-    path: path,
-    pageTitle: 'Add Product',
-    pathStr: '/admin/add-product',
-    errorMessage: '',
-    oldInput: {
-      title: '',
-      imageURL: '',
-      price: 0,
-      description: ''
-    },
-    fieldErrorsObj: {
-      title: false,
-      imageURL: false,
-      price: false,
-      description: false
-    }
+  const inputObj = {
+    response: response
   };
-   
-  response.render(path.join('admin', 'add-product.ejs'), optionsObj);
+
+  adminRenderer.renderAddProductPage(inputObj);
 };
 
 exports.postAddProduct = (request, response, next) => {
@@ -39,51 +26,14 @@ exports.postAddProduct = (request, response, next) => {
   }
 
   function _renderAddProductPage(errorsArr) {
-    const optionsObj = {
-      path: path,
-      pageTitle: 'Add Product',
-      pathStr: '/admin/add-product',
-      errorMessage: _getErrorMsg(errorsArr),
-      oldInput: {
-        title: request.body.title,
-        imageURL: request.body.imageURL,
-        price: request.body.price,
-        description: request.body.description
-      },
-      fieldErrorsObj: _getFieldErrorsObj(errorsArr)
-    };
-     
-    response.status(442).render(path.join('admin', 'add-product.ejs'), optionsObj);
-  }
-
-  function _getErrorMsg(errorsArr) {
-    const messagesArr = [];
-    
-    for(let i = 0; i < errorsArr.length; i++) {
-      const messageStr = errorsArr[i].msg;
-    
-      messagesArr.push(messageStr);
-    }
-    
-    return messagesArr.join('. ');
-  }
-
-  function _getFieldErrorsObj(errorsArr) {
-    const fieldErrorsObj = {
-      title: false,
-      imageURL: false,
-      price: false,
-      description: false
+    const inputObj = {
+      request: request,
+      response: response,
+      status: 442,
+      errorsArr: errorsArr
     };
   
-    for (let i = 0; i < errorsArr.length; i++) {
-      const errorObj = errorsArr[i];
-      const path = errorObj['path'];
-  
-      fieldErrorsObj[path] = true;
-    }
-  
-    return fieldErrorsObj;
+    adminRenderer.renderAddProductPage(inputObj);
   }
 
   function _addProduct() {
@@ -115,21 +65,12 @@ exports.getEditProductPage = (request, response, next) => {
   .catch(err => console.log(err));
   
   function _renderEditProductPage(product, response) {
-    const optionsObj = {
-      path: path,
-      pageTitle: 'Edit Product',
-      pathStr: '/admin/edit-product',
-      errorMessage: '',
-      product: product,
-      fieldErrorsObj: {
-        title: false,
-        imageURL: false,
-        price: false,
-        description: false
-      }
+    const inputObj = {
+      response: response,
+      product: product
     };
-
-    response.render(path.join('admin', 'edit-product.ejs'), optionsObj);
+  
+    adminRenderer.renderEditProductPage(inputObj); 
   }
 };
 
@@ -147,53 +88,14 @@ exports.postEditProduct = (request, response, next) => {
   }
   
   function _renderEditProductPage(errorsArr) {
-    const optionsObj = {
-      path: path,
-      pageTitle: 'Edit Product',
-      pathStr: '/admin/edit-product',
-      errorMessage: _getErrorMsg(errorsArr),
-      product: {
-        _id: request.body.productID,
-        title: request.body.title,
-        imageURL: request.body.imageURL,
-        price: request.body.price,
-        description: request.body.description,
-        userID: request.user._id,
-      },
-      fieldErrorsObj: _getFieldErrorsObj(errorsArr)
-    };
-
-    response.status(442).render(path.join('admin', 'edit-product.ejs'), optionsObj);
-  }
-
-  function _getErrorMsg(errorsArr) {
-    const messagesArr = [];
-    
-    for(let i = 0; i < errorsArr.length; i++) {
-      const messageStr = errorsArr[i].msg;
-    
-      messagesArr.push(messageStr);
-    }
-    
-    return messagesArr.join('. ');
-  }
-
-  function _getFieldErrorsObj(errorsArr) {
-    const fieldErrorsObj = {
-      title: false,
-      imageURL: false,
-      price: false,
-      description: false
+    const inputObj = {
+      request: request,
+      response: response,
+      status: 442,
+      errorsArr: errorsArr
     };
   
-    for (let i = 0; i < errorsArr.length; i++) {
-      const errorObj = errorsArr[i];
-      const path = errorObj['path'];
-  
-      fieldErrorsObj[path] = true;
-    }
-  
-    return fieldErrorsObj;
+    adminRenderer.renderEditProductPage(inputObj);
   }
 
   function _editProduct() {
@@ -234,14 +136,12 @@ exports.getProducts = (request, response, next) => {
   .catch(err => console.log(err));
   
   function _renderAdminProductsPage(arr) {
-    const optionsObj = {
-      path: path,
-      pageTitle: 'Admin Products',
-      pathStr: '/admin/products',
-      prods: arr
+    const inputObj = {
+      response: response,
+      arr: arr
     };
   
-    response.render(path.join('admin', 'products.ejs'), optionsObj);
+    adminRenderer.renderAdminProductsPage(inputObj);
   }
 };
 
