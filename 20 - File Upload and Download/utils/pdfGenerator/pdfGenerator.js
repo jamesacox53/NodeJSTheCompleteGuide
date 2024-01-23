1a93105eb453f1bdb64b2ab9709c53072f69df38
jamesacox53 <jamesacox53@gmail.com>
2024-01-23 20:20:52 +0000

    Completed Generating .pdf Files with Order Data

--- 20 - File Upload and Download/utils/pdfGenerator/pdfGenerator.js
+++ 20 - File Upload and Download/utils/pdfGenerator/pdfGenerator.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const pdfkit = require('pdfkit');
 
-const generateInvoicePDF = function(inputObj) {
+function generateInvoicePDF(inputObj) {
     const response = inputObj.response;
     const orderID = inputObj.order._id.toString();
     
@@ -12,17 +12,47 @@ const generateInvoicePDF = function(inputObj) {
     response.setHeader('Content-Type', 'application/pdf');
     response.setHeader('Content-Disposition', 'inline; filename="' + invoiceNameStr + '"');
 
-    return _generateInvoicePDF(response, invoicePath);
+    return _generateInvoicePDF(inputObj, response, invoicePath);
 }
 
-const _generateInvoicePDF = function(response, invoicePath) {
+function _generateInvoicePDF(inputObj, response, invoicePath) {
     const pdfDoc = new pdfkit(); 
     
     pdfDoc.pipe(fs.createWriteStream(invoicePath));
     pdfDoc.pipe(response);
 
-    pdfDoc.text('Hello world!');
+    _addContentToPDF(inputObj, pdfDoc);
+
     return pdfDoc.end();
+}
+
+function _addContentToPDF(inputObj, pdfDoc) {
+    const order = inputObj.order;
+    const productsArr = order.products;
+    
+    pdfDoc.fontSize(26).text('Invoice');
+    pdfDoc.fontSize(16).text(' ');
+    
+    _addOrderLines(pdfDoc, productsArr);
+}
+
+function _addOrderLines(pdfDoc, productsArr) {
+    let sum = 0;
+
+    for (let i = 0; i < productsArr.length; i++) {
+        const product = productsArr[i];
+        const title = product.productID.title;
+        const price = product.productID.price;
+        const quantity = product.quantity;
+        const totalPrice = price *  quantity;
+
+        const orderText = `${title} - ${quantity} x $${price} = ${totalPrice}`; 
+
+        pdfDoc.fontSize(16).text(orderText);
+        sum += totalPrice;
+    }
+
+    pdfDoc.fontSize(16).text(`Sum: ${sum}`);
 }
 
 exports.generateInvoicePDF = generateInvoicePDF;
