d011c2d608a9f7d4a5b5fb1e827d64ae548c6fc0
jamesacox53 <jamesacox53@gmail.com>
2024-01-26 21:24:48 +0000

    Completed Manipulating the DOM

--- 22 - Understanding Async Requests/public/js/admin.js
+++ 22 - Understanding Async Requests/public/js/admin.js
@@ -1,16 +1,38 @@
 const deleteProduct = (btn) => {
     const productID = btn.parentNode.querySelector('[name=productID]').value;
     const csrf = btn.parentNode.querySelector('[name=CSRFToken]').value;
+    if (!(_checkArgs(productID, csrf))) return;
 
-    const routeStr = '/admin/delete-product/' + productID;
-    const fetchOpts = {
-        method: 'DELETE',
-        headers: {
-            'x-csrf-token': csrf
-        }
-    };
-
-    fetch(routeStr, fetchOpts)
-    .then(res => console.log(res))
+    _sendDelRequest(productID, csrf)
+    .then(response => _getDataFromResponse(response))
+    .then(data => _removeProductFromDOM(data))
     .catch(err => console.log(err));
+
+    function _checkArgs(productID, csrf) {
+        if (!productID || !csrf) return false;
+
+        return true;
+    }
+
+    function _sendDelRequest(productID, csrf) {
+        const routeStr = '/admin/delete-product/' + productID;
+        const fetchOpts = {
+            method: 'DELETE',
+            headers: {
+                'x-csrf-token': csrf
+            }
+        };
+
+        return fetch(routeStr, fetchOpts);
+    }
+
+    function _getDataFromResponse(response) {
+        return response.json();
+    }
+
+    function _removeProductFromDOM(data) {
+        const productElem = btn.closest('article');
+
+        productElem.parentNode.removeChild(productElem);
+    }
 }
