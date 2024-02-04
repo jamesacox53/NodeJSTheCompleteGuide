5ee666985fa309fc51e6ab54d42614083fb2ef3b
jamesacox53 <jamesacox53@gmail.com>
2024-02-04 16:23:03 +0000

    Completed Using & Validating the Token

--- 25 - Working with Rest APIs/routes/feedRoutes.js
+++ 25 - Working with Rest APIs/routes/feedRoutes.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+const isAuth = require(path.join('..', 'utils', 'middleware', 'isAuth.js'));
 const feedValidators = require(path.join('..', 'utils', 'validators', 'feedValidators.js'));
 const feedController = require(path.join('..', 'controllers', 'feedController.js'));
 
 const router = express.Router();
 
 router.get('/post/:postID', feedController.getPost);
-router.get('/feed/posts', feedController.getPosts);
+router.get('/feed/posts', isAuth, feedController.getPosts);
 
 router.put('/post/:postID', feedValidators.editPostValidatorArr, feedController.putEditPost);
 router.delete('/post/:postID', feedController.deletePost);
