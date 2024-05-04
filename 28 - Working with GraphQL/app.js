fe8ffc22ec852f1cacab855d1b6d2c63ceb298f6
jamesacox53 <jamesacox53@gmail.com>
2024-05-04 20:31:02 +0100

    Completed Understanding the Setup & Writing our First Query

--- 28 - Working with GraphQL/app.js
+++ 28 - Working with GraphQL/app.js
@@ -5,17 +5,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const { createHandler } = require("graphql-http/lib/use/express");
+const { ruruHTML } = require("ruru/server");
 
 const rootDirectoryStr = path.dirname(require.main.filename);
 const connectionStr = require(path.join(rootDirectoryStr, 'sensitive', 'mongooseDBConnectionStr.js'));
 const corsHeaders = require(path.join(rootDirectoryStr, 'utils', 'middleware', 'corsHeaders.js'));
 const errorHandler = require(path.join(rootDirectoryStr, 'utils', 'errorHandlers', 'errorHandler.js'));
 const multerOpts = require(path.join(rootDirectoryStr, 'utils', 'multerOpts', 'multerOpts.js'));
-const socket = require(path.join(rootDirectoryStr, 'utils', 'sockets', 'socket.js'));
 
-const authRoutes = require(path.join(rootDirectoryStr, 'routes', 'authRoutes.js'));
-const userRoutes = require(path.join(rootDirectoryStr, 'routes', 'userRoutes.js'));
-const feedRoutes = require(path.join(rootDirectoryStr, 'routes', 'feedRoutes.js'));
+const schema = require(path.join(rootDirectoryStr, 'graphql', 'schema.js'));
+const root = require(path.join(rootDirectoryStr, 'graphql', 'resolvers.js'));
+
 const fileStorage = multer.diskStorage(multerOpts.fileStorageObj);
 
 const app = express();
@@ -24,16 +25,21 @@ app.use(multer({ storage: fileStorage, fileFilter: multerOpts.fileFilterFunc }).
 app.use('images', express.static(path.join(rootDirectoryStr, 'images')));
 app.use(corsHeaders);
 
-app.use(authRoutes);
-app.use(userRoutes);
-app.use(feedRoutes);
-
+app.all("/graphql", createHandler({
+        schema: schema,
+        rootValue: root }))
+   
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html")
+  res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+   
 errorHandler.addErrorHandlers(app);
 
 const server = http.createServer(app);
 
 mongoose.connect(connectionStr)
 .then(res => {
-    const activeServer = server.listen(8080);
-    socket.initialize(activeServer);
+    server.listen(8080);
 });
