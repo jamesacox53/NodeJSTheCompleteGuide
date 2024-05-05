5e7b03956aac3a8df50ab5e51473579ddad58a96
jamesacox53 <jamesacox53@gmail.com>
2024-05-05 20:15:05 +0100

    Completed Defining a Mutation Schema

--- 28 - Working with GraphQL/app.js
+++ 28 - Working with GraphQL/app.js
@@ -5,7 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const { createHandler } = require("graphql-http/lib/use/express");
 const { ruruHTML } = require("ruru/server");
 
 const rootDirectoryStr = path.dirname(require.main.filename);
@@ -13,9 +12,7 @@ const connectionStr = require(path.join(rootDirectoryStr, 'sensitive', 'mongoose
 const corsHeaders = require(path.join(rootDirectoryStr, 'utils', 'middleware', 'corsHeaders.js'));
 const errorHandler = require(path.join(rootDirectoryStr, 'utils', 'errorHandlers', 'errorHandler.js'));
 const multerOpts = require(path.join(rootDirectoryStr, 'utils', 'multerOpts', 'multerOpts.js'));
-
-const schema = require(path.join(rootDirectoryStr, 'graphql', 'schema.js'));
-const root = require(path.join(rootDirectoryStr, 'graphql', 'resolvers.js'));
+const graphQLOpts = require(path.join(rootDirectoryStr, 'utils', 'graphQLHandler', 'graphQLHandler.js'));
 
 const fileStorage = multer.diskStorage(multerOpts.fileStorageObj);
 
@@ -24,16 +21,13 @@ app.use(bodyParser.json());
 app.use(multer({ storage: fileStorage, fileFilter: multerOpts.fileFilterFunc }).single('image'));
 app.use('images', express.static(path.join(rootDirectoryStr, 'images')));
 app.use(corsHeaders);
+app.all("/graphql", graphQLOpts);
 
-app.all("/graphql", createHandler({
-        schema: schema,
-        rootValue: root }))
-   
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
   res.type("html")
   res.end(ruruHTML({ endpoint: "/graphql" }))
-})
+});
    
 errorHandler.addErrorHandlers(app);
 
---
+++ 28 - Working with GraphQL/utils/graphQLHandler/graphQLHandler.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const { createHandler } = require("graphql-http/lib/use/express");
+
+const rootDirectoryStr = path.dirname(require.main.filename);
+
+const schema = require(path.join(rootDirectoryStr, 'graphQL', 'schema.js'));
+const root = require(path.join(rootDirectoryStr, 'graphQL', 'resolvers.js'));
+
+module.exports = createHandler({
+    schema: schema,
+    rootValue: root 
+});
