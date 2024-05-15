eb9a15c5083bf881614b80f4f468e782af837589
jamesacox53 <jamesacox53@gmail.com>
2024-05-15 20:54:34 +0100

    Completed Organizing Multiple Tests

--- 30 - Testing Node.js Applications/test/auth-middleware.js
+++ 30 - Testing Node.js Applications/test/auth-middleware.js
@@ -1,12 +1,25 @@
 import { expect } from 'chai';
 import isAuth from '../utils/middleware/isAuth.js';
 
-it ('should throw an error if no Authorization header is present', function() {
-    const req = {
-        get: function() {
-            return null;
-        }
-    };
-
-    expect(isAuth.bind(this, req, {}, () => {})).to.throw("Authorization header doesn't exist.");
+describe('Auth Middleware', function() {
+    it ('should throw an error if no Authorization header is present', function() {
+        const req = {
+            get: function() {
+                return null;
+            }
+        };
+    
+        expect(isAuth.bind(this, req, {}, () => {})).to.throw("Authorization header doesn't exist.");
+    });
+    
+    it ('should throw an error if the Authorization header is only one string', function() {
+        const req = {
+            get: function() {
+                return 'xyz';
+            }
+        };
+    
+        expect(isAuth.bind(this, req, {}, () => {})).to.throw("Authorization header should be in the format of 'Bearer jwtStr'.");
+    });
 });
+
