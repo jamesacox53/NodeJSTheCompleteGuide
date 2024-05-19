836789d2a4281331255f6b9811cffa2aa67f91e2
jamesacox53 <jamesacox53@gmail.com>
2024-05-19 20:26:50 +0100

    Completed Node Core Modules & Promises

--- 32 - Modern JavaScript & NodeJS/app.js
+++ 32 - Modern JavaScript & NodeJS/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import resHandler from './response-handler.js';
+import { resHandler, resHandler2 } from './response-handler.js';
 
 const app = express();
 
 app.get('/', resHandler);
+app.get('/2', resHandler2);
 
 app.listen(3000);
--- 32 - Modern JavaScript & NodeJS/response-handler.js
+++ 32 - Modern JavaScript & NodeJS/response-handler.js
@@ -1,4 +1,4 @@
-// import fs from 'fs';
+import fs from 'fs/promises';
 
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
@@ -7,11 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const resHandler = (req, res, next) => {
-  res.sendFile(join(__dirname, 'my-page.html'));  
-  
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-      // res.send(data);
-    // });
+  res.sendFile(join(__dirname, 'my-page.html'));
 };
 
-export default resHandler;
+const resHandler2  = (req, res, next) => {  
+  fs.readFile('my-page.html', 'utf8')
+  .then(data => res.send(data))
+  .catch(err => console.log(err));
+};
+
+export { resHandler, resHandler2 };
