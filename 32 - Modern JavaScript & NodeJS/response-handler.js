0a81630bd7a2791130197b7712dca106801ab176
jamesacox53 <jamesacox53@gmail.com>
2024-05-19 20:15:16 +0100

    Completed More on ES Modules

--- 32 - Modern JavaScript & NodeJS/response-handler.js
+++ 32 - Modern JavaScript & NodeJS/response-handler.js
@@ -1,9 +1,17 @@
-import fs from 'fs';
+// import fs from 'fs';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const resHandler = (req, res, next) => {
-    fs.readFile('my-page.html', 'utf8', (err, data) => {
-      res.send(data);
-    });
+  res.sendFile(join(__dirname, 'my-page.html'));  
+  
+  // fs.readFile('my-page.html', 'utf8', (err, data) => {
+      // res.send(data);
+    // });
 };
 
 export default resHandler;
