ec624f61e5ab3f5513f4cc8dcb2b4c1d0e9d4ba6
jamesacox53 <jamesacox53@gmail.com>
2024-05-19 13:04:07 +0100

    Completed Hooks

--- 30 - Testing Node.js Applications/test/user-controller.js
+++ 30 - Testing Node.js Applications/test/user-controller.js
@@ -9,45 +9,52 @@ import UserController from '../controllers/userController.js';
 import User from '../models/user.js';
 
 describe('User Controller', function() {
-    it('should send a response with a valid user status for an existing user', function(done) {
+    before(function(done) {
         mongoose.connect(mongooseTestDBConnectionStr)
         .then(res => {
-            return new User({
+            const user = new User({
                 _id: '5c0f66b979af55031b34728a',
                 email: 'test@test.com',
                 password: 'testertester',
                 name: 'Test',
                 status: 'Test Status',
                 post: []
-            }).save();
-        })
-        .then(() => {
-            const req = { 
-                params: {
-                    userID: '5c0f66b979af55031b34728a'
-                }
-            };
-
-            const res = {
-                statusCode: 500,
-                userStatus: null,
-                status: function (code) {
-                    this.statusCode = code;
-                    return this;
-                },
-                json: function(data) {
-                    this.userStatus = data.status;
-                }
-            };
-
-            UserController.getStatus(req, res, () => {}).then(() => {
-                expect(res.statusCode).to.be.equal(200);
-                expect(res.userStatus).to.be.equal('Test Status');
-                User.deleteMany({})
-                .then(() => mongoose.disconnect())
-                .then(() => done());
             });
+
+            return user.save();
         })
-        .catch(err => console.log(err));
+        .then(() => done());
+    });
+
+    it('should send a response with a valid user status for an existing user', function(done) {
+        const req = { 
+            params: {
+                userID: '5c0f66b979af55031b34728a'
+            }
+        };
+
+        const res = {
+            statusCode: 500,
+            userStatus: null,
+            status: function (code) {
+                this.statusCode = code;
+                return this;
+            },
+            json: function(data) {
+                this.userStatus = data.status;
+            }
+        };
+
+        UserController.getStatus(req, res, () => {}).then(() => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(res.userStatus).to.be.equal('Test Status');
+            done();
+        }).catch(err => console.log(err));
+    });
+
+    after(function(done) {
+        User.deleteMany({})
+        .then(() => mongoose.disconnect())
+        .then(() => done());
     });
 });
