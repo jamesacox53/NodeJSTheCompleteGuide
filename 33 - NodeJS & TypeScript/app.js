9748b5ae70e506d8b0ba22120308e1b3a23dae2c
jamesacox53 <jamesacox53@gmail.com>
2024-05-20 13:47:38 +0100

    Completed Using Object & Array Types

--- 33 - NodeJS & TypeScript/app.js
+++ 33 - NodeJS & TypeScript/app.js
@@ -2,6 +2,8 @@
 const num1Elem = document.getElementById('num1');
 const num2Elem = document.getElementById('num2');
 const buttonElem = document.querySelector('button');
+const numResults = [];
+const textResults = [];
 function myAdd(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -13,11 +15,19 @@ function myAdd(num1, num2) {
         return (+num1 + +num2);
     }
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElem.addEventListener('click', () => {
     const num1 = num1Elem.value;
     const num2 = num2Elem.value;
     const res = myAdd(+num1, +num2);
+    numResults.push(res);
+    const text = myAdd(num1, num2);
+    textResults.push(text);
     console.log(res);
+    console.log(printResult({ val: res, timestamp: new Date() }));
+    console.log(numResults, textResults);
 });
 console.log(myAdd(1, 6));
 console.log(myAdd('1', '6'));
--- 33 - NodeJS & TypeScript/app.ts
+++ 33 - NodeJS & TypeScript/app.ts
@@ -2,6 +2,9 @@ const num1Elem = document.getElementById('num1') as HTMLInputElement;
 const num2Elem = document.getElementById('num2') as HTMLInputElement;
 const buttonElem = document.querySelector('button')!;
 
+const numResults: number[] = [];
+const textResults: string[] = [];
+
 function myAdd(num1: number | string, num2: number | string) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -14,13 +17,23 @@ function myAdd(num1: number | string, num2: number | string) {
     }
 }
 
+function printResult(resultObj: { val: number; timestamp: Date }) {
+    console.log(resultObj.val);
+}
+
 buttonElem.addEventListener('click', () => {
     const num1 = num1Elem.value;
     const num2 = num2Elem.value;
 
     const res = myAdd(+num1, +num2);
+    numResults.push(res as number);
+
+    const text = myAdd(num1, num2);
+    textResults.push(text as string);
 
     console.log(res);
+    console.log(printResult({ val: res as number, timestamp: new Date() }));
+    console.log(numResults, textResults);
 });
 
 console.log(myAdd(1, 6));
