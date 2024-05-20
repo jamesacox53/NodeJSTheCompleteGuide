c1d4e5a361578420374cd56238d3177eab16ba31
jamesacox53 <jamesacox53@gmail.com>
2024-05-20 11:43:06 +0100

    Completed Type Inference & Type Casting

--- 33 - NodeJS & TypeScript/app.js
+++ 33 - NodeJS & TypeScript/app.js
@@ -1,5 +1,14 @@
-function add(num1, num2) {
+var num1Elem = document.getElementById('num1');
+var num2Elem = document.getElementById('num2');
+var buttonElem = document.querySelector('button');
+function myAdd(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 6));
-// console.log(add('1', '6'));
+buttonElem === null || buttonElem === void 0 ? void 0 : buttonElem.addEventListener('click', function () {
+    var num1 = num1Elem.value;
+    var num2 = num2Elem.value;
+    var res = myAdd(+num1, +num2);
+    console.log(res);
+});
+console.log(myAdd(1, 6));
+// console.log(myAdd('1', '6'));
--- 33 - NodeJS & TypeScript/app.ts
+++ 33 - NodeJS & TypeScript/app.ts
@@ -1,7 +1,20 @@
-function add(num1: number, num2: number) {
+const num1Elem = document.getElementById('num1') as HTMLInputElement;
+const num2Elem = document.getElementById('num2') as HTMLInputElement;
+const buttonElem = document.querySelector('button');
+
+function myAdd(num1: number, num2: number) {
     return num1 + num2;
 }
 
-console.log(add(1, 6));
+buttonElem?.addEventListener('click', () => {
+    const num1 = num1Elem.value;
+    const num2 = num2Elem.value;
 
-// console.log(add('1', '6'));
+    const res = myAdd(+num1, +num2);
+
+    console.log(res);
+});
+
+console.log(myAdd(1, 6));
+
+// console.log(myAdd('1', '6'));
