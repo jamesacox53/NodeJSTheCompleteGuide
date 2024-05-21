768096881a77942a8f43ff4a10ff14a960c4a8eb
jamesacox53 <jamesacox53@gmail.com>
2024-05-21 20:29:58 +0100

    Completed Adding REST Routes with TypeScript

--- 33 - NodeJS & TypeScript/Project2/app.ts
+++ 33 - NodeJS & TypeScript/Project2/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import todosRoutes from './routes/todos.js'
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(todosRoutes)
 
