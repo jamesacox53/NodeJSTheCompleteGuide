43a4ec3806f9ce1d744a6ae50bf6f01455de4d97
jamesacox53 <jamesacox53@gmail.com>
2024-05-25 15:44:33 +0100

    Completed Using the MongoDB Client Module

--- 35 - Deno CRUD & Databases/helpers/db_client.ts
+++ 35 - Deno CRUD & Databases/helpers/db_client.ts
@@ -1,11 +1,12 @@
-import { MongoClient, Database } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
+import { MongoClient, Database } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
 import mongoURL from '../sensitive/mongoDBCreds.ts';
 
 let db: Database;
 
-export function connect() {
+export async function connect() {
     const client = new MongoClient();
-    client.connectWithUri(mongoURL);
+    // client.connectWithUri(mongoURL);
+    await client.connect(mongoURL);
 
     db = client.database('todo-app');
 }
--- 35 - Deno CRUD & Databases/routes/todos.ts
+++ 35 - Deno CRUD & Databases/routes/todos.ts
@@ -1,28 +1,59 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { ObjectId } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
+
 
 import { getDb } from '../helpers/db_client.ts';
+
 
 const router = new Router();
 
 interface Todo {
-  id: string;
+  id?: string;
   text: string;
 }
 
-let todos: Todo[] = [];
+interface MongoTodo {
+  _id: ObjectId;
+  text: string;
+}
 
-router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos };
+router.get('/todos', async (ctx) => {
+  const todos = await getDb().collection('todos');
+  const todosArr = await todos.find({ text: { $ne: null } }).toArray();
+  
+  const transformedTodos = _getTransformedTodosArr(todosArr);
+  
+  ctx.response.body = { todos: transformedTodos };
+
+  function _getTransformedTodosArr(todosArr: MongoTodo[]) {
+    const newTodosArr: Todo[] = [];
+    
+    for (let i = 0; i < todosArr.length; i++) {
+      const todo = todosArr[i];
+      
+      const todoObj = {
+        id: todo._id.toString(),
+        text: todo.text
+      };
+
+      newTodosArr.push(todoObj);
+    }
+    
+    return newTodosArr;
+  }
 });
 
 router.post('/todos', async (ctx) => {
-  const data = await ctx.request.body();
+  const req = await ctx.request.body.json();
+  const text = req.text; 
+
   const newTodo: Todo = {
-    id: new Date().toISOString(),
-    text: data.value.text,
+    text: text,
   };
 
-  todos.push(newTodo);
+  const id = await getDb().collection('todos').insertOne(newTodo);
+
+  newTodo.id = id.toString();
 
   ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
